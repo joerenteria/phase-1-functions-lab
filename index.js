f4a4918e864ed8f7e91422b29c84232ee5900d3c
Joe Renteria <joe@electronflight.com>
2021-07-21 21:40:16 -0700

    commit tree 546ef72c86a7b9a772e0411ed1e789ef941793f2

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+    let distance = 0;
+  
+      if (pickupLocation > 42){
+      distance = pickupLocation - 42;
+      }
+      if (pickupLocation < 42){
+      distance = 42 - pickupLocation;
+      }
+      return distance;
+  }
+
+
+  function distanceFromHqInFeet(pickupLocation){
+    let distance = 0;
+  
+      if (pickupLocation > 42){
+      distance = (pickupLocation - 42) * 264;
+      }
+      if (pickupLocation < 42){
+      distance = (42 - pickupLocation) * 264;
+      }
+      return distance;
+  }
+
+  function distanceTravelledInFeet(begin,end){    
+    let length = (begin - end)* 264;
+    return Math.abs(length);
+}
+
+
+function calculatesFarePrice(start,destination){    
+    let length = (start - destination)* 264;
+    Math.abs(length);
+
+    
+    if(length < 400){
+        return 0;
+      }
+
+    else if(length < 2000){
+      return (length - 400) * 2 / 100;
+    }
+
+    else if(length < 2500){
+      return 25;
+    }
+
+    else if(length > 2500){
+        return `cannot travel that far`;
+      }
+
+  }
